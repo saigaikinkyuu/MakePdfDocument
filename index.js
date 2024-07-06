0057d8042bf3c7a90a0849e419ab8913f53fd1b1
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-07-06 19:06:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("button").addEventListener("click", () => {
     const content = document.querySelector("#content");
     const opt = {
-      margin:       1,
+      margin:       0,
       filename:     new Date().getFullYear() + "" + ("0" + new Date().getMonth()).slice(-2) + "" + ("0" + new Date().getDate()).slice(-2) + "" + ("0" + new Date().getHours()).slice(-2) + "" + ("0" + new Date().getMinutes()).slice(-2) + "" + ("0" + new Date().getSeconds()).slice(-2) + '.pdf',
       image:        { type: 'jpeg', quality: 0.98 },
       html2canvas:  { scale: 2 },
