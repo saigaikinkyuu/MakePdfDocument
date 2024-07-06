4fa6ac2679b0fd5adc1c4c0b847b87533f381727
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-07-06 18:58:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("button").addEventListener("click", () => {
     const content = document.querySelector("#content");
-    const filename = "sample.pdf";
-    html2pdf(content).save(filename);
+    const opt = {
+      margin:       1,
+      filename:     'sample.pdf',
+      image:        { type: 'jpeg', quality: 0.98 },
+      html2canvas:  { scale: 2 },
+      jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+    html2pdf().from(content).set(opt).save();
   });
 });
