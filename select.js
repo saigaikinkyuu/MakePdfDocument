3e4a78319ccd513522d883a9f7a331a3ca7f4e10
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-07-07 14:49:50 +0900

    Create select.js

---
+++ select.js
@@ -0,0 +1,10 @@
+function coppyId() {
+  const select = document.getElementById('serect');
+  select.addEventListener('change', (e) => {
+  //コピーするテキストを取得
+  let getTxt = e.target.value;
+ 
+  //取得したテキストをクリップボードに書き込み
+  navigator.clipboard.writeText(getTxt);
+  });
+}
