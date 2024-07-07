365bef0e962240dcc62c291f3a2941b18b0cd0fd
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-07-07 13:58:21 +0900

    Update quake.js

--- quake.js
+++ quake.js
@@ -48,7 +48,7 @@ $.getJSON("https://www.jma.go.jp/bosai/quake/data/list.json", function (data) {
                   hypo = body.Earthquake.Hypocenter.Area.Name
                 }
                 if(body.Earthquake.Hypocenter.Area.Coordinate){
-                  depth = ((body.Earthquake.Hypocenter.Area.Coordinate).substr((body.Earthquake.Hypocenter.Area.Coordinate).indexOf('-') + 1)).substr(0, ((body.Earthquake.Hypocenter.Area.Coordinate).substr((body.Earthquake.Hypocenter.Area.Coordinate).indexOf('-') + 1)).indexOf('/')) + "㎞";
+                  depth = (((body.Earthquake.Hypocenter.Area.Coordinate).substr((body.Earthquake.Hypocenter.Area.Coordinate).indexOf('-') + 1)).substr(0, ((body.Earthquake.Hypocenter.Area.Coordinate).substr((body.Earthquake.Hypocenter.Area.Coordinate).indexOf('-') + 1)).indexOf('/')))/1000 + "㎞";
                 }
               }
             }
