59e5959cfdd4b897b5c925b449f06adb9e1b7c0d
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-07-22 08:45:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const content = document.querySelector("#content");
     const opt = {
       margin:       0,
-      filename:     new Date().getFullYear() + "" + ("0" + new Date().getMonth()).slice(-2) + "" + ("0" + new Date().getDate()).slice(-2) + "" + ("0" + new Date().getHours()).slice(-2) + "" + ("0" + new Date().getMinutes()).slice(-2) + "" + ("0" + new Date().getSeconds()).slice(-2) + '.pdf',
+      filename:     new Date().getFullYear() + "" + ("0" + (new Date().getMonth()+1)).slice(-2) + "" + ("0" + new Date().getDate()).slice(-2) + "" + ("0" + new Date().getHours()).slice(-2) + "" + ("0" + new Date().getMinutes()).slice(-2) + "" + ("0" + new Date().getSeconds()).slice(-2) + '.pdf',
       image:        { type: 'jpeg', quality: 0.98 },
       html2canvas:  { scale: 2 },
       jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
