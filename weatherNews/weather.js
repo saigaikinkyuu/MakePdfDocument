e73c61cffb209bbdad6c315d8aa214050d3a37e8
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-08-06 20:40:14 +0900

    Update weather.js

--- weatherNews/weather.js
+++ weatherNews/weather.js
@@ -1,1 +1,16 @@
+// Fetch APIを使用した例
+fetch("https://www.data.jma.go.jp/developer/xml/feed/extra.xml")
+    .then(response => response.text())
+    .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+    .then(xmlDoc => {
+        const entries = xmlDoc.getElementsByTagName("entry");
+        for (let i = 0; i < entries.length; i++) {
+          if(entries[i].getElementsByTagName("title") && entries[i].getElementsByTagName("updated") && entries[i].getElementsByTagName("id") && entries[i].getElementsByTagName("author").getElementsByTagName("name") && entries[i].getElementsByTagName("link") && entries[i].getElementsByTagName("content")){
+            //
+          }
+        }
+    })
+    .catch(error => {
+        console.error("Failed to load XML file:", error);
+    });
 
