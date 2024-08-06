cd8551f3c5080da92cb0ae4857c382d8feec72d0
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-08-06 20:48:02 +0900

    Update weather.js

--- weatherNews/weather.js
+++ weatherNews/weather.js
@@ -4,11 +4,14 @@ fetch("https://www.data.jma.go.jp/developer/xml/feed/extra.xml")
     .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
     .then(xmlDoc => {
         const entries = xmlDoc.getElementsByTagName("entry");
+        let dataContent = ""
         for (let i = 0; i < entries.length; i++) {
           if(entries[i].getElementsByTagName("title") && entries[i].getElementsByTagName("updated") && entries[i].getElementsByTagName("id") && entries[i].getElementsByTagName("author").getElementsByTagName("name") && entries[i].getElementsByTagName("link") && entries[i].getElementsByTagName("content")){
-            //
+            document.getElementById('serect').innerHTML = ""
+            dataContent += "<option value='" + entries[i].getElementsByTagName("id") + "'>" + "[" + entries[i].getElementsByTagName("title") + "]" + date(entries[i].getElementsByTagName("updated")) + "(" + entries[i].getElementsByTagName("author").getElementsByTagName("name") + ")"
           }
         }
+        document.getElementById('serect').innerHTML = dataContent
     })
     .catch(error => {
         console.error("Failed to load XML file:", error);
