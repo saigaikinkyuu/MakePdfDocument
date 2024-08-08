b5acb0dfcb02f5b6ec62ea8c63bed61de98d5840
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-08-08 16:07:41 +0900

    Update select.js

--- select.js
+++ select.js
@@ -1,8 +1,9 @@
 const select = document.getElementById('serect')
-  select.addEventListener('onclick', (e) => {
-  //コピーするテキストを取得
-  let getTxt = e.target.value;
+  select.addEventListener('change', (e) => {
+    document.getElementById("example").style.display = "none"
+    //コピーするテキストを取得
+    let getTxt = e.target.value;
  
-  //取得したテキストをクリップボードに書き込み
-  navigator.clipboard.writeText(getTxt);
+    //取得したテキストをクリップボードに書き込み
+    navigator.clipboard.writeText(getTxt);
   });
