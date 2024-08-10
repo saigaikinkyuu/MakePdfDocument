b73439b42569f642f795bd126c91c7871ef2c290
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-08-10 10:42:36 +0900

    Update quake.js

--- quake/quake.js
+++ quake/quake.js
@@ -40,7 +40,7 @@ $.getJSON("https://www.jma.go.jp/bosai/quake/data/list.json", function (data) {
       let hypo = "調査中"
       let coment1 = "調査中"
       let coment2 = "調査中"
-      if(control.Title !== "南海トラフ地震臨時情報"){
+      if(control.Title !== "南海トラフ地震臨時情報" || control.Title !== "顕著な地震の震源要素更新のお知らせ"){
       if(control.Title){
           if(control.EditorialOffice){
             rpOffice = control.EditorialOffice
@@ -282,6 +282,16 @@ $.getJSON("https://www.jma.go.jp/bosai/quake/data/list.json", function (data) {
         document.getElementById("title" + s).innerHTML = head.Title
         document.getElementById("date" + s).innerHTML = date(head.TargetDateTime) + "発表"
         document.getElementById("body" + s).innerHTML = head.Headline.Text + "\n" + body.EarthquakeInfo.Text 
+      }else if(control.Title === "顕著な地震の震源要素更新のお知らせ"){
+        document.getElementById("title" + s).innerHTML = head.Title
+	document.getElementById("date" + s).innerHTML = date(head.TargetDateTime) + "発表"
+	let depth = ""
+	if((body.Earthquake.Hypocenter.Area.Coordinate_WGS).slice(-3) === "+0/"){
+	  depth = "ごく浅い"
+	}else {
+          depth = (((body.Earthquake.Hypocenter.Area.Coordinate_WGS).substr((body.Earthquake.Hypocenter.Area.Coordinate_WGS).indexOf('-') + 1)).substr(0, ((body.Earthquake.Hypocenter.Area.Coordinate_WGS).substr((body.Earthquake.Hypocenter.Area.Coordinate_WGS).indexOf('-') + 1)).indexOf('/')))/1000 + "㎞";
+	}
+	document.getElementById("body" + s).innerHTML = head.Headline.Text + "\n更新された情報です。\n" + (date(body.Earthquake.ArrivalTime)).slice(-10) + "ころ、" + body.Earthquake.Hypocenter.Area.Name + "にて、地震がありました。震源の深さは" + depth + "、マグニチュードは" + body.Earthquake.Magnitude + "となっています。"
       }
     }
   })
