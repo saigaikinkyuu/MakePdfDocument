c7d1b3fcd3984b614c82a0c60f8d41646769a39b
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-08-12 09:40:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("button").addEventListener("click", () => {
+    let pathname = location.pathname
+    pathname.replace(/[/ ]/g,"")
+    pathname.replace(/[index.html ]/g,"")
     const content = document.querySelector("#content");
     const opt = {
       margin:       0,
-      filename:     new Date().getFullYear() + "" + ("0" + (new Date().getMonth()+1)).slice(-2) + "" + ("0" + new Date().getDate()).slice(-2) + "" + ("0" + new Date().getHours()).slice(-2) + "" + ("0" + new Date().getMinutes()).slice(-2) + "" + ("0" + new Date().getSeconds()).slice(-2) + '.pdf',
+      filename:     "[" + pathname + "]" + new Date().getFullYear() + "" + ("0" + (new Date().getMonth()+1)).slice(-2) + "" + ("0" + new Date().getDate()).slice(-2) + "" + ("0" + new Date().getHours()).slice(-2) + "" + ("0" + new Date().getMinutes()).slice(-2) + "" + ("0" + new Date().getSeconds()).slice(-2) + '.pdf',
       image:        { type: 'jpeg', quality: 0.98 },
       html2canvas:  { scale: 2 },
       jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
