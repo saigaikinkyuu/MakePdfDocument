379c463244b33d551dee14fb055791589f1566e1
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-08-13 09:24:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
     let pathname = location.pathname
     pathname = pathname.replace(/[/ ]/g,"")
     pathname = pathname.replace("MakePdfDocument","")
+    let headName = document.getElementById("title").textContent
     const content = document.querySelector("#content");
     const opt = {
       margin:       0,
-      filename:     "[" + pathname + "]" + new Date().getFullYear() + "" + ("0" + (new Date().getMonth()+1)).slice(-2) + "" + ("0" + new Date().getDate()).slice(-2) + "" + ("0" + new Date().getHours()).slice(-2) + "" + ("0" + new Date().getMinutes()).slice(-2) + "" + ("0" + new Date().getSeconds()).slice(-2) + '.pdf',
+      filename:     "[" + pathname + "]" + headName + "(" + new Date().getFullYear() + "" + ("0" + (new Date().getMonth()+1)).slice(-2) + "" + ("0" + new Date().getDate()).slice(-2) + "" + ("0" + new Date().getHours()).slice(-2) + "" + ("0" + new Date().getMinutes()).slice(-2) + "" + ("0" + new Date().getSeconds()).slice(-2) + ').pdf',
       image:        { type: 'jpeg', quality: 0.98 },
       html2canvas:  { scale: 2 },
       jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
