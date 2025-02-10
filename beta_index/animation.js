bf467500199c4fd70e7f3fe122895b50948797f6
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2025-02-10 17:42:03 +0900

    Update animation.js

--- beta_index/animation.js
+++ beta_index/animation.js
@@ -1,12 +1,20 @@
-document.getElementById("display_cont_list").addEventListener("mouseover", (event) => {
-  const height_1 = document.getElementById('display_cont_list').clientHeight
-  const height_2 = document.getElementById('edit_hover').clientHeight
-  document.getElementById('edit_hover').style.padding = ((height_1 - height_2) / 2) + "px 0px " + ((height_1 - height_2) / 2) + "px 0px"
-  document.getElementById("display_cont_list").style.display = "none"
-  document.getElementById("edit_hover").style.display = "block"
+document.getElementById("lists_cov").addEventListener("mouseover", (event) => {
+  document.getElementById("lists_cov").style.opacity = 0
+  (seTimeout () =>{
+    document.getElementById("lists_cov").style.opacity = 1
+    const height_1 = document.getElementById('display_cont_list').clientHeight
+    const height_2 = document.getElementById('edit_hover').clientHeight
+    document.getElementById('edit_hover').style.padding = ((height_1 - height_2) / 2) + "px 0px " + ((height_1 - height_2) / 2) + "px 0px"
+    document.getElementById("display_cont_list").style.display = "none"
+    document.getElementById("edit_hover").style.display = "block"
+  }, 250)
 });
 
-document.getElementById("display_cont_list").addEventListener("mouseout", (event) => {
-  document.getElementById("display_cont_list").style.display = "block"
-  document.getElementById("edit_hover").style.display = "none"
+document.getElementById("lists_cov").addEventListener("mouseout", (event) => {
+  document.getElementById("lists_cov").style.opacity = 0
+  (seTimeout () =>{
+    document.getElementById("lists_cov").style.opacity = 1
+    document.getElementById("display_cont_list").style.display = "block"
+    document.getElementById("edit_hover").style.display = "none"
+  }, 250)
 });
