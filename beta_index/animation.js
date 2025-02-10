e295cc343128e186b4edcd79e741da2ee9323002
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2025-02-10 17:47:42 +0900

    Update animation.js

--- beta_index/animation.js
+++ beta_index/animation.js
@@ -1,6 +1,6 @@
 document.getElementById("lists_cov").addEventListener("mouseover", (event) => {
   document.getElementById("lists_cov").style.opacity = 0
-  seTimeout(() =>{
+  setTimeout(() =>{
     const height_1 = document.getElementById('display_cont_list').clientHeight
     const height_2 = document.getElementById('edit_hover').clientHeight
     document.getElementById('edit_hover').style.padding = ((height_1 - height_2) / 2) + "px 0px " + ((height_1 - height_2) / 2) + "px 0px"
@@ -12,7 +12,7 @@ document.getElementById("lists_cov").addEventListener("mouseover", (event) => {
 
 document.getElementById("lists_cov").addEventListener("mouseout", (event) => {
   document.getElementById("lists_cov").style.opacity = 0
-  seTimeout(() =>{
+  setTimeout(() =>{
     document.getElementById("display_cont_list").style.display = "block"
     document.getElementById("edit_hover").style.display = "none"
     document.getElementById("lists_cov").style.opacity = 1
