9a50dc41a6a4528791e6142a9a6e6801e038ae7b
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2025-02-10 18:00:46 +0900

    Update animation.js

--- beta_index/animation.js
+++ beta_index/animation.js
@@ -1,12 +1,12 @@
 document.getElementById("lists_cov").addEventListener("mouseover", (event) => {
   document.getElementById("lists_cov").style.opacity = 0
   setTimeout(() =>{
+    document.getElementById("edit_hover").style.display = "block"
     const height_1 = document.getElementById('display_cont_list').clientHeight
     const height_2 = document.getElementById('edit_hover').clientHeight
-    console.log(height_1,height_2)
     document.getElementById('edit_hover').style.padding = ((height_1 - height_2) / 2) + "px 0px " + ((height_1 - height_2) / 2) + "px 0px"
+    document.getElementById('edit_hover').style.margin = "30px 10px 30px 10px"
     document.getElementById("display_cont_list").style.display = "none"
-    document.getElementById("edit_hover").style.display = "block"
     document.getElementById("lists_cov").style.opacity = 1
   }, 250)
 });
